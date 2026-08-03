9070b381e42fea693a28d0830cd4754dc8037f39
Simon Wade <simon.wade@gmail.com>
2026-08-03 16:58:31 +1000

    Keep RSS observation metadata refresh responsive

--- packages/agentgraph-connector-rss/agentgraph_connector_rss/__init__.py
+++ packages/agentgraph-connector-rss/agentgraph_connector_rss/__init__.py
@@ -41,7 +41,7 @@ from agentgraph_connector_rss.auth import (
 _STALE_AFTER = 30 * 60
 _FEED_TIMEOUT = httpx.Timeout(10.0, connect=5.0)
 _MAX_FEED_BYTES = 5_000_000
-_MAX_OBSERVATION_ENTRIES_PER_FEED = 64
+_MAX_OBSERVATION_ENTRIES_PER_FEED = 8
 _MAX_OBSERVATION_PATTERNS_PER_FEED = 5
 _TRACKING_QUERY_KEYS = {
     "fbclid",
